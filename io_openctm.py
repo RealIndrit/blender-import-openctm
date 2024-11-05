555645b11ed03730210675f48c2fb23660946ef9
RealIndrit <externalcontact.shield824@passmail.net>
2024-11-05 02:15:43 +0100

    Cooked UVs

--- io_openctm.py
+++ io_openctm.py
@@ -32,7 +32,6 @@ class OpenCTMImport(bpy.types.Operator, ImportHelper):
                 raise IOError("Error loading file: %s" % str(ctmErrorString(err)))
 
             mesh = bpy.data.meshes.new(name="ImportedMesh")
-            mesh_obj = bpy.data.objects.new(name="ImportedObject", object_data=mesh)
 
             # read vertices
             vertex_count = ctmGetInteger(ctm_context, CTM_VERTEX_COUNT)
@@ -50,36 +49,38 @@ class OpenCTMImport(bpy.types.Operator, ImportHelper):
 
             mesh.from_pydata(vertices=vertices, edges=[], faces=faces)
 
-            # Validate mesh
-            mesh.validate()
+            print(len(mesh.vertices), len(mesh.polygons))
 
             # Could be cooked, UNTESTED
             if ctmGetInteger(ctm_context, CTM_UV_MAP_COUNT) > 0:
                 for map_index in range(8):
-                    uv_layer = mesh.uv_layers.new(name=f"UV{map_index}")
                     uv_coords = ctmGetFloatArray(ctm_context, (0x0700 + map_index))
                     if uv_coords:
-                        uvs = np.fromiter(uv_coords, count=face_count, dtype=float).reshape((-1, 2))
+                        uv_name = ctmGetUVMapString(ctm_context, (0x0700 + map_index), CTM_NAME)
+                        uv_name = uv_name.decode("utf-8") + f"{map_index}"
+                        uv_layer = mesh.uv_layers.new(name=f"UV{uv_name}")
+                        uvs = np.fromiter(uv_coords, count=vertex_count * 2, dtype=float).reshape((-1, 2))
 
-                        for vertexIndex in range(len(uvs)):
-                            cur_uvs = uvs[vertexIndex]
-                            uv_layer.data[vertexIndex].uv = cur_uvs
+                        for i, loop in enumerate(mesh.loops):
+                            uv_layer.data[loop.index].uv = uvs[i % len(uvs)]
 
             # Get colour map UNTESTED
-            colour_map = ctmGetNamedAttribMap(ctm_context, c_char_p(_encode('Color')))
-            if colour_map != CTM_FALSE:
-                colours = ctmGetFloatArray(ctm_context, colour_map)
-                colours = np.array(colours).reshape(-1, 4)
-
-                if colours:
-                    for color_3_index in range(len(colours)):
-                        color_3_layer = mesh.vertex_colors.new(name=f"rgb{color_3_index}")
-                        cur_col_3 = colours[color_3_index]
-                        color_3_layer.data[color_3_index].color = [cur_col_3[0], cur_col_3[1], cur_col_3[2], 0]
+            # https://github.com/Puxtril/CSV-Import/blob/master/ImportCSV.py#L347
+            # colour_map = ctmGetNamedAttribMap(ctm_context, c_char_p(_encode('Color')))
+            # if colour_map != CTM_FALSE:
+            #     colours = ctmGetFloatArray(ctm_context, colour_map)
+            #     colours = np.array(colours, count=vertex_count * 4, dtype=float).reshape((-1, 4))
+            #     if colours:
+            #         for color_3_index in range(len(colours)):
+            #             color_3_layer = mesh.vertex_colors.new(name=f"rgb{color_3_index}")
+            #             cur_col_3 = colours[color_3_index]
+            #             color_3_layer.data[color_3_index].color = [cur_col_3[0] *255, cur_col_3[1] *255, cur_col_3[2] *255, cur_col_3[3] *255]
 
             # Update the mesh with new data
             mesh.update()
 
+
+            mesh_obj = bpy.data.objects.new(name="ImportedObject", object_data=mesh)
             bpy.context.scene.collection.objects.link(mesh_obj)
             # Select the imported object
             mesh_obj.select_set(True)
