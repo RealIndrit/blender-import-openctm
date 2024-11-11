0f30cf3ef380c9777734b18fef2236eeb4da2cb1
RealIndrit <externalcontact.shield824@passmail.net>
2024-11-11 18:19:35 +0100

    [+] Template for blender 3.6 support

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,20 @@
 import bpy
+
+import io_openctm
 from .io_openctm import OpenCTMImport, OpenCTMExport
+
+bl_info = {
+    "name": "OpenCTM (.ctm)",
+    "description": "Import/Export meshes from/to OpenCTM (.ctm)",
+    "author": "RealIndrit",
+    "version": (1, 2, 0),
+    "blender": (3, 6, 0),
+    "location": "File > Import-Export",
+    "warning": "",
+    "wiki_url": "https://github.com/RealIndrit/blender-import-openctm",
+    "tracker_url": "https://github.com/RealIndrit/blender-import-openctm",
+    "category": "Import-Export",
+}
 
 def menu_import(self, context):
     self.layout.operator(OpenCTMImport.bl_idname, text="OpenCTM (.ctm)")
@@ -8,15 +23,13 @@ def menu_export(self, context):
     self.layout.operator(OpenCTMExport.bl_idname, text="OpenCTM (.ctm)")
 
 def register():
-    bpy.utils.register_class(OpenCTMImport)
+    io_openctm.register()
     bpy.types.TOPBAR_MT_file_import.append(menu_import)
-    bpy.utils.register_class(OpenCTMExport)
     bpy.types.TOPBAR_MT_file_export.append(menu_export)
 
 def unregister():
-    bpy.utils.unregister_class(OpenCTMImport)
+    io_openctm.unregister()
     bpy.types.TOPBAR_MT_file_import.remove(menu_import)
-    bpy.utils.unregister_class(OpenCTMExport)
     bpy.types.TOPBAR_MT_file_export.remove(menu_export)
 
 if __name__ == "__main__":
